ce1c63736edf09f0dc804cdab07bf66dd193f964
dermatobia <webblewebble@gmail.com>
2014-06-06 23:45:46 -0400

    work in progress

--- prob10.rb
+++ prob10.rb
@@ -7,7 +7,7 @@
 def primes(num)
   whole_array = (2..num).to_a
   prime_array = [whole_array.shift]
-
+  sum = 0
   until whole_array == []
     whole_array.delete_if { |x| x % prime_array.last == 0 }
     prime_array << whole_array.shift
@@ -20,4 +20,19 @@ def sum_primes(num)
   prime_array.reduce(:+)
 end
 
-p sum_primes(1_000_000)
+# p sum_primes(1_000_000)
+# p primes(10)
+
+def sum(num)
+  whole_array = (2..num).to_a
+  last_prime = whole_array.shift
+  sum = last_prime
+  until whole_array == []
+    whole_array.delete_if {|x| x % last_prime == 0 }
+    last_prime = whole_array.shift
+    sum += last_prime
+  end
+  sum
+end
+
+p sum(2_000_000)
